f147bb71ea27a53676042221507c488d7e2fa566
Henrique Yule <yzhenrique@gmail.com>
2023-09-12 22:20:24 -0400

    Adicionando rota para envio de audio

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import { fastify } from "fastify";
 import { prisma } from "./lib/prisma";
 import { getAllPrompts } from "./routes/prompts";
+import { uploadVideo } from "./routes/video";
 
 const app = fastify();
 
 app.register(getAllPrompts)
+app.register(uploadVideo)
 
 app.listen({
     port: 3333,
